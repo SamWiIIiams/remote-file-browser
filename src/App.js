52f5fdfe7aeb6505d6746f1a98feeb712365e8f4
unknown <sam.m.williams23@gmail.com>
2022-04-23 00:16:56 -0400

    clearing out project

--- src/App.js
+++ src/App.js
@@ -3,41 +3,9 @@ import React, { useState, useRef, useEffect } from 'react';
 import Directory from './components/Directory'
 import { v4 as uuidv4 } from 'uuid'
 
-const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  const [todos, setTodos] = useState([])
-  const todoNameRef = useRef()
   
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (storedTodos) setTodos(storedTodos)
-  }, [])
-  
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }, [todos])
-  
-  function handleAddTodo(e) {
-    const name = todoNameRef.current.value
-    if (name === '') return
-    setTodos(prevTodos => {
-      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
-    })
-    todoNameRef.current.value = null
-  }
-  
-  return (
-    <>
-      <Directory todosList = {todos}/>
-      <input ref={todoNameRef} type="text"/>
-      <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Completed Todos</button>
-      <div>0 left to do</div>
-    </>
-  )
-
-
 }
 
 export default App;
