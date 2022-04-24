d0853ad3aedcdde45f6409a570e2c22e618ba399
unknown <sam.m.williams23@gmail.com>
2022-04-24 14:16:05 -0400

    some stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import './App.css';
 import React, { useState, useRef, useEffect } from 'react';
-import Directory from './components/Directory'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFolder } from '@fortawesome/free-solid-svg-icons'
 import { v4 as uuidv4 } from 'uuid'
 
+const directory = {
+    name: "teleport",
+    size: 0, // bytes
+    type: "dir",
+    items: [
+      {
+        name: "lib",
+        size: 0, // bytes
+        type: "dir",
+        items: [
+          {
+            name: "teleport.go",
+            size: 320000, // bytes
+            type: "file",
+          },
+          {
+            name: "test.go",
+            size: 3320000, // bytes
+            type: "file",
+          },
+        ],
+      },
+      {
+        name: "README.md",
+        size: 4340000, // bytes
+        type: "file",
+      },
+    ],
+  };
 
 function App() {
-  
+    
+
+  return (
+    <div>
+        <ul>
+            {directory.items.find(item => item.name === "lib").items.map((item, idx) =>
+                <li key={idx}>
+                    <div><FontAwesomeIcon icon={faFolder}/> {item.name}</div>
+                    <div class="itemSize">{item.size}</div>
+                </li>
+            )}
+        </ul>
+    </div>
+  )
 }
 
 export default App;
