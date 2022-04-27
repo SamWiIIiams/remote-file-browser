0b3e39b4728edda892d65f9b37c593973edb3800
unknown <sam.m.williams23@gmail.com>
2022-04-27 01:02:56 -0400

    formatting prettier

--- src/App.js
+++ src/App.js
@@ -1,60 +1,55 @@
-import './App.css';
-import React, { useState, useRef, useEffect } from 'react';
-import { Routes, Link, Route, Navigate, useLocation } from 'react-router-dom'
-import { getItems, getRoot } from './api';
+import "./App.css";
+import React, { useState, useRef, useEffect } from "react";
+import { Routes, Link, Route, Navigate, useLocation } from "react-router-dom";
+import { getItems, getRoot } from "./api";
 //import Folder from './components/Folder';
 
 //const dirName = window.location.pathname;
 
-
 export function Folder() {
-    const { pathname } = useLocation();
-    const path = pathname.endsWith('/') ? pathname.slice(0, -1) : pathname;
-    const data = getItems(path);
-    
-    return (
-        <div>
-            <h1>{data.name}</h1>
-            <ul>
-                {data.items.map((item) => (
-                    <li key={item.name}>
-                        {item.type === "file" && (
-                            <>
-                                {item.name}, {item.size}
-                            </>
-                        )}
-                        {item.type === "dir" && (
-                            <>
-                                <Link to={path + "/" + item.name}>
-                                    {item.name}, {item.type}
-                                </Link>
-                                
-                            </>
-                        )}
-                    </li>
-                ))}
-            </ul>
-       
+  const { pathname } = useLocation();
+  const path = pathname.endsWith("/") ? pathname.slice(0, -1) : pathname;
+  const data = getItems(path);
 
-        </div>
-    );
-};
-
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <ul>
+        {data.items.map((item) => (
+          <li key={item.name}>
+            {item.type === "file" && (
+              <>
+                {item.name}, {item.size}
+              </>
+            )}
+            {item.type === "dir" && (
+              <>
+                <Link to={path + "/" + item.name}>
+                  {item.name}, {item.type}
+                </Link>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 function App() {
   const root = getRoot();
 
   return (
-      <>
+    <>
       <div>
         <h2>TeleBrowser</h2>
       </div>
-      <hr/>
+      <hr />
       <Routes>
         <Route path="/" element={<Navigate to={root} />} />
         <Route path="/:path/*" element={<Folder />} />
       </Routes>
-      </>
+    </>
   );
 }
 
--- src/api.js
+++ src/api.js
@@ -1,79 +1,77 @@
 const directory = {
-    name: "teleport",
-    size: 0, // bytes
-    type: "dir",
-    items: [
+  name: "teleport",
+  size: 0, // bytes
+  type: "dir",
+  items: [
+    {
+      name: "lib",
+      size: 0, // bytes
+      type: "dir",
+      items: [
         {
-            name: "lib",
-            size: 0, // bytes
-            type: "dir",
-            items: [
-                {
-                    name: "teleport.go",
-                    size: 320000, // bytes
-                    type: "file",
-                },
-                {
-                    name: "test.go",
-                    size: 3320000, // bytes
-                    type: "file",
-                },
-                {
-                    name: "new",
-                    size: 0,
-                    type: "dir",
-                    items: [
-                        {
-                            name: "newThing",
-                            size: 3423231,
-                            type: "file"
-                        }
-                    ],
-                },
-            ],
+          name: "teleport.go",
+          size: 320000, // bytes
+          type: "file",
         },
         {
-            name: "README.md",
-            size: 4340000, // bytes
-            type: "file",
+          name: "test.go",
+          size: 3320000, // bytes
+          type: "file",
         },
         {
-            name: "bin",
-            size: 0, // bytes
-            type: "dir",
-            items: [
-                {
-                    name: "binfile.md",
-                    size: 439798, // bytes
-                    type: "file",
-                },
-            ],
+          name: "new",
+          size: 0,
+          type: "dir",
+          items: [
+            {
+              name: "newThing",
+              size: 3423231,
+              type: "file",
+            },
+          ],
         },
-        
-    ],
+      ],
+    },
+    {
+      name: "README.md",
+      size: 4340000, // bytes
+      type: "file",
+    },
+    {
+      name: "bin",
+      size: 0, // bytes
+      type: "dir",
+      items: [
+        {
+          name: "binfile.md",
+          size: 439798, // bytes
+          type: "file",
+        },
+      ],
+    },
+  ],
 };
 
-  export function getItems(newPath) {
-      if (newPath.endsWith('/')) {newPath = newPath.slice(0, -1)}
-      console.log("new path = " + newPath);
-     
-          let pathArray = newPath.split("/")
-          pathArray.shift();
-          console.log(pathArray)
-          let search = directory;
-          for (let i = 1; i < pathArray.length; i++) {
-              search = search.items.filter((dir) => dir.type === 'dir').find(item => item.name === pathArray[i]);
-          }
-
-          return search;
-      
+export function getItems(newPath) {
+  if (newPath.endsWith("/")) {
+    newPath = newPath.slice(0, -1);
   }
-  export function getRoot() {
-      return "/" + directory.name;
+  console.log("new path = " + newPath);
+
+  let pathArray = newPath.split("/");
+  pathArray.shift();
+  console.log(pathArray);
+  let search = directory;
+  for (let i = 1; i < pathArray.length; i++) {
+    search = search.items
+      .filter((dir) => dir.type === "dir")
+      .find((item) => item.name === pathArray[i]);
   }
 
-  export function getData(url) {
+  return search;
+}
+export function getRoot() {
+  return "/" + directory.name;
+}
 
-  }
-
-  
+export function getData(url) {}
